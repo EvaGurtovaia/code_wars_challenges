012d987e8682512d2c0808e903fabb5476dc953b
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-07-14 13:45:16 -0500

    find the missing letter challenge

--- July.js
+++ July.js
@@ -38,3 +38,30 @@ const summation = (num) => {
     }
     return sum;
 };
+
+//Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+
+let findMissingLetter = arr => {  
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    if (arr[0] === arr[0].toUpperCase()){
+      alphabet = alphabet.toUpperCase();
+      console.log(alphabet)
+    }
+    const startingPosition = alphabet.indexOf(arr[0]);
+  
+    for (let i = startingPosition; i < startingPosition + arr.length; i++){
+    if (alphabet[i] !== arr[i - startingPosition]){
+      return alphabet[i];
+    }; 
+  }
+  }
