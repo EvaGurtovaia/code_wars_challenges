c101facdcdadc0c1f0e3d56c3a485a49992f3951
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-07-23 03:01:50 -0500

    highest and lowest challenge

--- July.js
+++ July.js
@@ -180,18 +180,43 @@ const rowSumOddNumbersTwo = (n) => Math.pow(n, 3);
 
 // The function takes a name as its only argument, and returns one of the following strings:
 
-// name + " plays banjo" 
+// name + " plays banjo"
 // name + " does not play banjo"
 
-const areYouPlayingBanjo = name => {
+const areYouPlayingBanjo = (name) => {
     let result;
     const letters = "r, R";
     if (letters.indexOf(name[0]) !== -1) {
-      result = name + " plays banjo"; 
-    } else { result = name + " does not play banjo";}
-      return result;
-    };
+        result = name + " plays banjo";
+    } else {
+        result = name + " does not play banjo";
+    }
+    return result;
+};
 
-const areYouPlayingBanjo2 = name => {
-    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
-  };
+const areYouPlayingBanjo2 = (name) => {
+    return (
+        name +
+        (name[0].toLowerCase() == "r" ? " plays" : " does not play") +
+        " banjo"
+    );
+};
+
+//Highest and Lowest
+
+//Description
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first
+
+const highAndLow = (numbers) => {
+    let nums = numbers.split(" ");
+    return `${Math.max(...nums)} ${Math.min(...nums)}`;
+};
