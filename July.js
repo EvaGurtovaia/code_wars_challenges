910dd84d297e9e89855e4f647df18138118f1698
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-07-24 13:14:14 -0500

    are they the same challenge

--- July.js
+++ July.js
@@ -221,7 +221,6 @@ const highAndLow = (numbers) => {
     return `${Math.max(...nums)} ${Math.min(...nums)}`;
 };
 
-
 //Begginer - Lost Without a Map
 
 // Given an array of integers, return a new array with each value doubled.
@@ -230,20 +229,19 @@ const highAndLow = (numbers) => {
 
 // [1, 2, 3] --> [2, 4, 6]
 
-const maps = x => {
+const maps = (x) => {
     let result = [];
-    for (let i = 0; i < x.length; i++){
-      result.push(x[i]*2);
+    for (let i = 0; i < x.length; i++) {
+        result.push(x[i] * 2);
     }
     return result;
-  };
+};
 
+const maps2 = (x) => {
+    return x.map((n) => n * 2);
+};
 
-const maps2 = x => {
-    return x.map(n => n * 2);
-  };
-
-  //Keep Hydrated!
+//Keep Hydrated!
 
 //Nathan loves cycling.
 // Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
@@ -253,9 +251,39 @@ const maps2 = x => {
 // time = 6.7---> litres = 3
 // time = 11.8--> litres = 5
 
-  const litres = time => {
-    return Math.floor(time * 0.5);   
-  };
+const litres = (time) => {
+    return Math.floor(time * 0.5);
+};
 
+//Are They The "Same"?
 
-  
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+// If, for example, we change the first number to something else, comp is not returning true anymore:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+const comp = (array1, array2) => {
+    if (!array1 || !array2 || array1.length !== array2.length) return false;
+    return (
+        array1
+            .map((num) => num * num)
+            .sort()
+            .toString() === array2.sort().toString()
+    );
+};
