ad8393ed33cc6c8fafd371b12335b473a31d7edd
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-07-27 12:18:41 -0500

    return negative challenge

--- July.js
+++ July.js
@@ -287,3 +287,28 @@ const comp = (array1, array2) => {
             .toString() === array2.sort().toString()
     );
 };
+
+//Return Negative
+
+// In this simple assignment you are given a number and have to make it negative.
+//But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+const makeNegative = (num) => {
+    let result;
+    if (num <= 0) return num;
+    else result = num * -1;
+    return result;
+};
+
+const makeNegative2 = (num) => {
+    return num < 0 ? num : -num;
+};
