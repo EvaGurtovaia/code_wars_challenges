e162d865d8302f81443c945736c06eef24ec762a
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-01 22:19:07 -0500

    selection sort implementation challenge

--- August.js
+++ August.js
@@ -86,3 +86,24 @@ var searchInsert = function (nums, target) {
     }
     return nums[middle] > target ? middle : middle + 1;
 };
+
+//Selection Sort Implementation
+
+const selectionSort = (arr) => {
+    for (let i = 0; i > arr.length; i++) {
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+        if (i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr;
+};
+
+console.log(selectionSort([8, 1, 2, 3, 4]));
