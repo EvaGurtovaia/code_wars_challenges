b1995a9994efb1a975a475a9de5d6ea62bcf998f
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-06 12:27:50 -0500

    Calculate BMI

--- August.js
+++ August.js
@@ -214,3 +214,34 @@ const getSum = (a, b) => {
     }
     return sum;
 };
+
+//Calculate BMI
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+const bmi = (weight, height) => {
+    let bmi = weight / (height * height);
+    if (bmi <= 18.5) {
+        return "Underweight";
+    } else if (bmi <= 25) {
+        return "Normal";
+    } else if (bmi <= 30) {
+        return "Overweight";
+    } else return "Obese";
+};
+
+const bmi2 = (weight, height) => {
+    let bmi = weight / (height * height);
+
+    return bmi < 18.5
+        ? "Underweight"
+        : bmi <= 25
+        ? "Normal"
+        : bmi <= 30
+        ? "Overweight"
+        : "Obese";
+};
