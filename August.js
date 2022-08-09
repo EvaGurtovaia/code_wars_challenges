d29caa47dec17ee99e9d0687db4bae150c8abff4
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-09 00:55:09 -0500

    array.diff challenge

--- August.js
+++ August.js
@@ -275,6 +275,7 @@ const findShort = (s) => {
 
 const firstNonRepeatedCharacter = (str) => {
     let chars = {};
+    let c;
     for (let i = 0; i < str.length; i++) {
         c = str[i];
         if (!chars[c]) chars[c] = 1;
@@ -289,3 +290,18 @@ const firstNonRepeatedCharacter = (str) => {
 };
 
 console.log(firstNonRepeatedCharacter("ABA"));
+
+//Array.diff
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+const arrayDiff = (a, b) => {
+    return a.filter((el) => !b.includes(el));
+};
