c9eb729ed0d62ff164a6a6253c5c9e8410bafc53
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-11 13:31:36 -0500

    power challenge

--- August.js
+++ August.js
@@ -318,4 +318,15 @@ function factorial(num) {
     if (num < 0) return 0;
     if (num <= 1) return 1;
     return num * factorial(num - 1);
-};
+}
+
+//Power Challenge
+
+// Write a function called power which accepts a base and an exponent.
+// The function should return the power of the base to the exponent.
+// This function should mimic the functionality of Math.pow()  - do not worry about negative bases and exponents
+
+function power(base, exponent) {
+    if (exponent === 0) return 1;
+    return base * power(base, exponent - 1);
+}
