8687ddef16f387c1cae21d1d2467260cf5d6b793
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-11 19:47:46 -0500

    fibonacci challenge

--- August.js
+++ August.js
@@ -366,3 +366,13 @@ function recursiveRange(num) {
     if (num === 0) return 0;
     return num + recursiveRange(num - 1);
 }
+
+//Fibonacci Challenge
+//Write a recursive function called fib which accepts a number
+//and returns the nth number in the Fibonacci sequence.
+//Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+function fib(index) {
+    if (index <= 2) return 1;
+    return fib(index - 2) + fib(index - 1);
+}
