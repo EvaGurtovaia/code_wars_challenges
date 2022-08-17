b9aaef144601328b49822b3e9c8deb36768a1b2b
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-17 03:20:10 -0500

    convert a number to a string challenge

--- August.js
+++ August.js
@@ -428,3 +428,19 @@ function mergeSort(arr) {
 }
 
 console.log(mergeSort([1, 10, 3, 4, 2, 6]));
+
+
+//Convert a Number to a String
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+    return num.toString();
+   }
