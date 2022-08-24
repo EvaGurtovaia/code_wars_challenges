f7c0b7a22e64b9162372a3b27a6d8a94f070bf07
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-24 13:57:36 -0500

    singly linked list unshift method challenge

--- August.js
+++ August.js
@@ -563,6 +563,19 @@ class SinglyLinkedList {
         }
         return current;
     }
+    ///Implement unshift method
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 let list = new SinglyLinkedList();
