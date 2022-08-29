c84a0b963e6612e8f09bfa6aaded2de5cc048c88
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-29 15:00:31 -0500

    doubly linked list pop method challenge

--- September.js
+++ September.js
@@ -29,6 +29,21 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+    ///Implement pop method
+    pop() {
+        if (!this.head) return undefined;
+        let oldTail = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = oldTail.prev;
+            this.tail.next = null;
+            oldTail.prev = null;
+        }
+        this.length--;
+        return oldTail;
+    }
 }
 
 list = new DoublyLinkedList();
