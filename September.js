279b94c6ddd092f0175b61bb4a0d1e8156597eb9
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-29 15:34:10 -0500

    doubly linked list shift method challenge

--- September.js
+++ September.js
@@ -44,6 +44,23 @@ class DoublyLinkedList {
         this.length--;
         return oldTail;
     }
+    //Implement shift method
+    shift() {
+        if (!this.head) return undefined;
+
+        let oldHead = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
 }
 
 list = new DoublyLinkedList();
