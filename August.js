01d91ecd8218fd88c0b0baa37f8bfa9c25cd7a8b
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-08-29 19:03:35 -0500

    number of segments in a string challenge

--- August.js
+++ August.js
@@ -721,3 +721,25 @@ let findContentChildren = function (g, s) {
     }
     return contentChildren;
 };
+
+
+//Number of Segments in a String
+
+// Given a string s, return the number of segments in the string.
+// A segment is defined to be a contiguous sequence of non-space characters.
+
+//  Example 1:
+
+// Input: s = "Hello, my name is John"
+// Output: 5
+// Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+// Example 2:
+
+// Input: s = "Hello"
+// Output: 1
+
+let countSegments = function(s) {
+    return s.split(" ").filter(item => item.length > 0).length;
+       
+   };
+
