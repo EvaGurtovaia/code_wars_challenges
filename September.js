8983731dd4828e80fc41b6f559892c324624056e
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-09-01 15:34:08 -0500

    isomorphic strings challenge

--- September.js
+++ September.js
@@ -99,7 +99,37 @@ const isSubsequence = (str1, str2) => {
          return true;}
      }
      return false;
-   }
+   };
+
+//Isomorphic Strings
+
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+// Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+
+   var isIsomorphic = function(s, t) {
+    let obj1 = {};
+    let obj2 = {};
+    for (let i = 0; i < s.length; i++) {
+         if (obj1[s[i]] !== obj2[t[i]])
+             return false;
+        else {
+            obj1[s[i]] = i;
+            obj2[t[i]] = i;
+        }
+    }
+    return true;
+    
+};
 
 
  
